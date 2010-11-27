60bd60b7bcfa75a0e71b527ace9fc94327ab2cd2
Valeriu Caraulean <caraulean@gmail.com>
2010-11-27 21:09:53 +0100

    To about view added an option to navigate to project home on github

--- mGitHub.SampleApp/ViewModels/AboutViewModel.cs
+++ mGitHub.SampleApp/ViewModels/AboutViewModel.cs
@@ -1,9 +1,26 @@
+using System;
 using Caliburn.Micro;
+using Microsoft.Phone.Tasks;
 
 namespace mGitHub.SampleApp.ViewModels
 {
-	public class AboutViewModel : Screen
+	public class AboutViewModel : Screen,
+	                                    ILaunchTask,
+	                                    IConfigureTask<WebBrowserTask>
+
 	{
-		
+		public event EventHandler<TaskLaunchEventArgs> TaskLaunchRequested = delegate { };
+		private string navigateToUrl = "";
+
+		public void ConfigureTask(WebBrowserTask task)
+		{
+			task.URL = navigateToUrl;
+		}
+
+		public void OpenBrowserWithProject()
+		{
+			navigateToUrl = "https://github.com/vcaraulean/mGitHub.SampleApp";
+			TaskLaunchRequested(this, TaskLaunchEventArgs.For<WebBrowserTask>());
+		}
 	}
 }
